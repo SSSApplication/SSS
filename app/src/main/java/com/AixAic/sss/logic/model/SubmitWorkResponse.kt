8a09af55b53f9d04fc710178844fbc7ece6cde8a
Aicccx <2746765350@qq.com>
2020-10-26 19:45:44 +0800

    test

--- app/src/main/java/com/AixAic/sss/logic/model/SubmitWorkResponse.kt
+++ app/src/main/java/com/AixAic/sss/logic/model/SubmitWorkResponse.kt
@@ -1,5 +1,5 @@
 package com.AixAic.sss.logic.model
 
-//提交任务表
-
-data class SubmitWork(val userName: String)
+//任务表
+// ownerName(发布人), workTitle(任务名称), describe(任务描述)
+data class WorkItem(val ownerName: String, val workTitle: String, val describe: String)
