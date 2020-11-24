e4ab871c4fe3d92ef09764ec69de49fe57bde64c
Aicccx <2746765350@qq.com>
2020-11-24 11:59:41 +0800

    任务提交界面

---
+++ app/src/main/java/com/AixAic/sss/ui/task/WorkSubmitActivity.kt
@@ -0,0 +1,12 @@
+package com.AixAic.sss.ui.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.AixAic.sss.R
+
+class WorkSubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_submit)
+    }
+}
