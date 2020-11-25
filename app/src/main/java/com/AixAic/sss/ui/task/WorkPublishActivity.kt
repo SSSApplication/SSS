11d2b80dd52e3ceceacce0d24fdc41dd6d468cb4
Aicccx <2746765350@qq.com>
2020-11-25 20:12:46 +0800

    任务接收、发布界面

--- app/src/main/java/com/AixAic/sss/ui/task/WorkPublishActivity.kt
+++ app/src/main/java/com/AixAic/sss/ui/task/WorkPublishActivity.kt
@@ -2,11 +2,20 @@ package com.AixAic.sss.ui.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 import com.AixAic.sss.R
 
 class WorkPublishActivity : AppCompatActivity() {
+
+
+    val departmentList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work_publish)
+
+        val typeList = ArrayList<String>()
+        val typeSpinner = findViewById<Spinner>(R.id.typeSpinner)
+
     }
 }
